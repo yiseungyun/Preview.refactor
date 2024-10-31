85e4262be597af35ed64b297cf23873d07332a7d
yiseungyun <seungxxyun@gmail.com>
2024-10-31 16:44:18 +0900

    feat: 프로젝트 초기 설정

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+      ],
+      'subject-case': [2, 'always', 'sentence-case'],
+      'subject-max-length': [2, 'always', 70],
+    },
+  };
