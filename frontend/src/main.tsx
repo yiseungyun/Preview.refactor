88b6d7c65ac67c0e03d9a37747256a86c0306856
JeongwooSeo <shipfriend4371@naver.com>
2024-11-03 18:07:18 +0900

    feat: React-router-dom 설치 및 라우터 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./routes.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} basename={"/"} />
+  </StrictMode>
+);
---
+++ frontend/src/routes.tsx
@@ -0,0 +1,7 @@
+import App from "./App.tsx";
+export const routes = [
+  {
+    element: <App />,
+    path: "/",
+  },
+];
