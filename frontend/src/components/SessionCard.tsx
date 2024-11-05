c2e156249bdca6138c1f0a6c398fbfed08b0b7d4
JeongwooSeo <shipfriend4371@naver.com>
2024-11-05 23:45:20 +0900

    feat: 세션 리스트 페이지와 세션 카드 컴포넌트 구현
    - UI만 구현한 상태

---
+++ frontend/src/components/SessionCard.tsx
@@ -0,0 +1,66 @@
+import { FaUserGroup } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa";
+interface Props {
+  category: string;
+  title: string;
+  host: string;
+  participant: number;
+  maxParticipant: number;
+  sessionStatus: "open" | "close";
+  questionListId: number;
+}
+
+const SessionCard = ({
+  category,
+  title,
+  host,
+  participant,
+  maxParticipant,
+  sessionStatus,
+  questionListId,
+}: Props) => {
+  return (
+    <li
+      className={
+        "relative flex rounded-2xl overflow-hidden border border-accent-gray py-3 max-w-3xl"
+      }
+    >
+      <div
+        className={`${sessionStatus === "open" ? "bg-accent-gray" : "bg-primary-active"} w-6 h-full absolute left-0 top-0`}
+        aria-label={"상태표시등"}
+      />
+      <div className={"flex-grow px-4 ml-6 flex flex-col gap-1 items-start"}>
+        <span
+          className={
+            "text-medium-xs border border-primary-dark rounded-2xl py-px px-2"
+          }
+        >
+          {category}
+        </span>
+        <h3 className={"text-semibold-r text-xl"}>{title}</h3>
+        <p className={"text-medium-r text-grayscale-400"}>
+          질문지인데 누르면 질문 리스트를 볼 수 있임 {questionListId}
+        </p>
+        <div className={"inline-flex items-center justify-between w-full"}>
+          <div className={"text-medium-s inline-flex items-center gap-1"}>
+            <span>{host} • </span>
+            <span className={"inline-flex items-center gap-1"}>
+              <FaUserGroup />{" "}
+              <span className={"inline-flex items-center"}>참여자</span>
+              {participant}/{maxParticipant}명
+            </span>
+          </div>
+          <button
+            className={
+              "text-semibold-s text-primary-dark hover:text-primary-dark-hover bg-transparent inline-flex items-center"
+            }
+          >
+            <span>참여하기</span> <FaArrowRight />
+          </button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default SessionCard;
---
+++ frontend/src/pages/SessionListPage.tsx
@@ -0,0 +1,135 @@
+import { FaCirclePlus } from "react-icons/fa6";
+import { useState } from "react";
+import SessionCard from "../components/SessionCard.tsx";
+
+interface Session {
+  id: number;
+  title: string;
+  category: string;
+  sessionStatus: "open" | "close";
+  host: {
+    nickname: string;
+  };
+  participant: number;
+  maxParticipant: number;
+}
+const SessionListPage = () => {
+  const [sessionList, setSessionList] = useState<Session[]>([
+    {
+      id: 1,
+      title: "프론트엔드 초보만 들어올 수 있음",
+      category: "프론트엔드",
+      sessionStatus: "open",
+      host: {
+        nickname: "J133",
+      },
+      participant: 1,
+      maxParticipant: 4,
+    },
+    {
+      id: 2,
+      title: "백엔드 초보만 들어올 수 있음",
+      category: "백엔드",
+      sessionStatus: "close",
+      host: {
+        nickname: "J000",
+      },
+      participant: 1,
+      maxParticipant: 2,
+    },
+  ]);
+  const [listLoading, setListLoading] = useState(false);
+
+  return (
+    <section
+      className={"flex flex-col gap-10 max-w-7xl w-screen h-screen p-20"}
+    >
+      <div>
+        <h1 className={"text-bold-l mb-6"}>스터디 세션 목록</h1>
+        <div className={"h-10 flex gap-2"}>
+          <input
+            className={"rounded-lg px-4 border h-full"}
+            type="text"
+            placeholder="세션을 검색하세요"
+          />
+          <select className={"rounded-lg px-4 border h-full"}>
+            <option>FE</option>
+            <option>BE</option>
+          </select>
+          <button
+            className={
+              "flex justify-center items-center fill-current text-white bg-primary hover:bg-primary-hover w-10 h-full rounded-lg"
+            }
+          >
+            <FaCirclePlus />
+          </button>
+          <button>링크입력</button>
+        </div>
+      </div>
+      <div>
+        <h2 className={"text-semibold-xl mb-6"}>공개된 세션 목록</h2>
+        <ul>
+          {listLoading ? (
+            <>loading</>
+          ) : (
+            <>
+              {sessionList.length <= 0 ? (
+                <li>아직 아무도 세션을 열지 않았어요..!</li>
+              ) : (
+                sessionList.map((session) => {
+                  return (
+                    session.sessionStatus === "open" && (
+                      <SessionCard
+                        key={session.id}
+                        sessionStatus={session.sessionStatus}
+                        category={session.category}
+                        title={session.title}
+                        host={session.host.nickname}
+                        questionListId={1}
+                        participant={session.participant}
+                        maxParticipant={session.maxParticipant}
+                      />
+                    )
+                  );
+                })
+              )}
+            </>
+          )}
+        </ul>
+      </div>
+      <div>
+        <h2 className={"text-semibold-xl mb-6"}>진행 중인 세션 목록</h2>
+        <ul>
+          {listLoading ? (
+            <>loading</>
+          ) : (
+            <>
+              {sessionList.length <= 0 ? (
+                <li>아직 아무도 세션을 열지 않았어요..!</li>
+              ) : (
+                sessionList.map((session) => {
+                  return (
+                    session.sessionStatus === "close" && (
+                      <SessionCard
+                        key={session.id}
+                        sessionStatus={session.sessionStatus}
+                        category={session.category}
+                        title={session.title}
+                        host={session.host.nickname}
+                        questionListId={1}
+                        participant={session.participant}
+                        maxParticipant={session.maxParticipant}
+                      />
+                    )
+                  );
+                })
+              )}
+            </>
+          )}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default SessionListPage;
