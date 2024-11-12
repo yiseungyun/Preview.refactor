1f52a397ecff1a10a066239cb79d839b9f0f2828
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-11-12 16:49:26 +0900

    feat: 토스트 컴포넌트에서 프로그레스바와 애니메이션 구현

---
+++ frontend/src/components/common/ProgressBar.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+
+interface ProgressBarProps {
+  duration: number;
+}
+
+const ProgressBar = ({ duration }: ProgressBarProps) => {
+  const progressRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const progressBar = progressRef.current;
+    if (progressBar) {
+      progressBar.style.animation = `expand ${duration}ms linear`;
+    }
+  }, [duration]);
+
+  return (
+    <div className={"absolute left-0 bottom-0 h-1 z-50  progressBar w-full"}>
+      <div
+        ref={progressRef}
+        className={"expand w-full origin-left bg-gray-50 h-full"}
+        aria-label={"프로그레스바 진행"}
+      ></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- frontend/src/components/common/Toast.tsx
+++ frontend/src/components/common/Toast.tsx
@@ -1,27 +1,27 @@
 import { IoIosClose } from "react-icons/io";
+import ProgressBar from "./ProgressBar.tsx";
 
 interface ToastProps {
   message: string;
   type: "success" | "error";
+  removeToast: () => void;
 }
 
-const Toast = ({ message, type }: ToastProps) => {
+const Toast = ({ message, type, removeToast }: ToastProps) => {
   return (
     <div
-      className={`relative flex items-center text-medium-s ${type === "error" ? "bg-red-400" : "bg-green-100"} overflow-hidden rounded-md text-white w-80 h-16 shadow-xl  p-2 z-50`}
+      className={`relative flex items-center text-medium-s ${type === "error" ? "bg-red-400" : "bg-green-100"} overflow-hidden text-white w-80 h-16 shadow-xl  p-2 z-50`}
     >
       <div className={"flex"}>
         <p className={"flex-grow"}>{message}</p>
-        <button className={"text-2xl absolute right-1 top-1"}>
+        <button
+          onClick={removeToast}
+          className={"text-2xl absolute right-1 top-1"}
+        >
           <IoIosClose />
         </button>
       </div>
-      <div className={"absolute left-0 bottom-0 h-1 z-50  progressBar w-full"}>
-        <div
-          className={"w-1/2 bg-gray-50 h-full"}
-          aria-label={"프로그레스바 진행"}
-        ></div>
-      </div>
+      <ProgressBar duration={5000} />
     </div>
   );
 };
