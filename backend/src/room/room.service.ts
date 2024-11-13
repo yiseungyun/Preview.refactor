043ebae0654736a4782c2693932184cb011d00e2
Chanwoo Kim <kcw2205@naver.com>
2024-11-13 22:45:46 +0900

    feat: 스터디 세션 서비스 추가
    - 스터디 세션 생성과 관련된 비즈니스 로직
    - `Redis` 등의 저장소 정보와 관심분리 목적

--- backend/src/room/room.service.ts
+++ backend/src/room/room.service.ts
@@ -1,25 +1,73 @@
 import { Injectable } from "@nestjs/common";
-import { RedisService } from "../redis/redis.service";
-import { Room } from "./room.model";
+import { MemberConnection, Room } from "./room.model";
 import { generateRoomId } from "../utils/generateRoomId";
 import { HOUR } from "../utils/time";
+import { RoomRepository } from "./room.repository";
+import { UpdateRoomDto } from "./dto/update-room.dto";
 
+/**
+ * 비즈니스 로직 처리를 좀 더 하게 하기 위한 클래스로 설정
+ * 예외 처리 (로직, 더 밑단 에러 처리 - redis 에러 등) 도 이곳에서 이루어질 예정
+ * redis 와 관련된 부분은 모르도록 하려고 합니다.
+ */
 @Injectable()
 export class RoomService {
-    constructor(private readonly redisService: RedisService) {}
+    private static MAX_MEMBERS = 5;
 
-    async createRoom(title: string, socketId: string) {
-        const client = this.redisService.getClient();
-        const roomId = generateRoomId();
+    constructor(private readonly roomRepository: RoomRepository) {}
 
-        await client.hset(`room:${roomId}`, {
-            title: title,
-            createdAt: Date.now(),
-            members: [socketId],
-            host: socketId,
-        } as Room);
+    async getPublicRoom() {
+        const rooms = await this.roomRepository.getAllRoom();
+        return rooms.filter((room) => room.status === "PUBLIC");
+    }
 
-        await client.expire(`room:${roomId}`, 6 * HOUR);
-        return roomId;
+    async createRoom(title: string, socketId: string, nickname: string) {
+        const roomId = await this.roomRepository.createRoom({
+            title,
+            status: "PUBLIC",
+            socketId,
+        });
+        await this.roomRepository.addUser(roomId, socketId, nickname, true);
+    }
+
+    async joinRoom(socketId: string, roomId: string, nickname: string) {
+        const room = this.roomRepository.getRoomById(roomId);
+        if (!room) return null; // throw join error
+        await this.roomRepository.addUser(roomId, socketId, nickname);
+    }
+
+    async getMemberSocket(roomId: string) {
+        console.log("겟멤버소켓 :", roomId);
+        const memberConnection =
+            await this.roomRepository.getRoomMemberConnection(roomId);
+        if (!memberConnection) return null;
+
+        return Object.keys(memberConnection);
+    }
+
+    async checkAvailable(roomId: string) {
+        const members =
+            await this.roomRepository.getRoomMemberConnection(roomId);
+        console.log(members);
+        return Object.keys(members).length < RoomService.MAX_MEMBERS;
+    }
+
+    async checkConnected(socketId: string) {
+        return this.roomRepository.findMyRoomId(socketId);
+    }
+
+    async disconnect(socketId: string) {
+        const roomId = await this.roomRepository.findMyRoomId(socketId);
+
+        if (!roomId) {
+            return;
+        }
+
+        await this.roomRepository.deleteUser(socketId);
+
+        const connections =
+            await this.roomRepository.getRoomMemberConnection(roomId);
+        console.log("커넥션 정보 : ", connections);
+        if (!connections) await this.roomRepository.deleteRoom(roomId);
     }
 }
