a4f2bd335739c73e4c8137d77a8990a9196a2e90
yiseungyun <seungxxyun@gmail.com>
2024-11-14 06:40:01 +0900

    refactor: 질문지 선택 모달창 카테고리 컴포넌트 분리

---
+++ frontend/src/components/sessions/create/SessionForm/ListSelectModal/CategoryTab/Category.tsx
@@ -0,0 +1,31 @@
+import useSessionFormStore from "../../../../../../stores/useSessionFormStore";
+
+interface Props {
+  tabName: "myList" | "savedList";
+  tabText: "내 질문지" | "저장된 질문지";
+}
+
+const Category = ({ tabName, tabText }: Props) => {
+  const { tab, setTab } = useSessionFormStore();
+
+  return (
+    <button
+      className={`flex flex-row gap-1
+      ${
+        tab === tabName
+          ? "text-bold-s text-green-500"
+          : "text-medium-m text-gray-400"
+      }`}
+      onClick={() => setTab(tabName)}
+    >
+      <span
+        className={`px-1 pb-1 border-b-2
+      ${tab === tabName ? "border-green-500" : "border-gray-white"}`}
+      >
+        {tabText}
+      </span>
+    </button>
+  );
+};
+
+export default Category;
--- frontend/src/components/sessions/create/SessionForm/ListSelectModal/CategoryTab/index.tsx
+++ frontend/src/components/sessions/create/SessionForm/ListSelectModal/CategoryTab/index.tsx
@@ -1,42 +1,10 @@
-import useSessionFormStore from "../../../../../../stores/useSessionFormStore";
+import Category from "./Category";
 
 const CategoryTap = () => {
-  const { tab, setTab } = useSessionFormStore();
-
   return (
     <section className="flex items-center gap-2">
-      <button
-        className={`flex flex-row gap-1
-          ${
-            tab === "myList"
-              ? "text-bold-s text-green-500"
-              : "text-medium-m text-gray-400"
-          }`}
-        onClick={() => setTab("myList")}
-      >
-        <span
-          className={`px-1 pb-1 border-b-2
-          ${tab === "myList" ? "border-green-500" : "border-gray-white"}`}
-        >
-          내 질문지
-        </span>
-      </button>
-      <button
-        className={`flex flex-row gap-1
-          ${
-            tab === "savedList"
-              ? "text-bold-s text-green-500"
-              : "text-medium-m text-gray-400"
-          }`}
-        onClick={() => setTab("savedList")}
-      >
-        <span
-          className={`px-1 pb-1 border-b-2
-          ${tab === "savedList" ? "border-green-500" : "border-gray-white"}`}
-        >
-          저장된 질문지
-        </span>
-      </button>
+      <Category tabName="myList" tabText="내 질문지" />
+      <Category tabName="savedList" tabText="저장된 질문지" />
     </section>
   );
 };
