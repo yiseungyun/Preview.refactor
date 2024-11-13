f187e89e372eb9c539af9e7d3bc91e1cad7af046
Chanwoo Kim <kcw2205@naver.com>
2024-11-13 22:43:30 +0900

    feat: 스터디 세션 모델 추가
    - MemberConnection : 각 방에 참여하는 유저의 연결 상세 정보
    - Room : 방의 상세정보(메타데이터)

--- backend/src/room/room.model.ts
+++ backend/src/room/room.model.ts
@@ -1,6 +1,15 @@
+export type RoomStatus = "PUBLIC" | "PRIVATE";
+
 export interface Room {
     title: string;
+    status: RoomStatus;
     createdAt: number;
     members: string[];
     host: string;
+}
+
+export interface MemberConnection {
+    joinTime: number;
+    isHost: boolean;
+    nickname: string;
 }
