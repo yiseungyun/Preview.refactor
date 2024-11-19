73269da1762dbfa99bf2823712de5d1393dbfcd5
JeongwooSeo <shipfriend4371@naver.com>
2024-11-19 17:09:39 +0900

    feat: 질문지 리스트 페이지 구현
    - 리스트 페이지 구현
    - 질문지 미리보기 컴포넌트 구현
    - 다크모드 지원
    - 반응형 레이아웃 지원

---
+++ frontend/src/pages/QuestionListPage.tsx
@@ -0,0 +1,93 @@
+import { FaPlus } from "react-icons/fa6";
+import Sidebar from "@components/common/Sidebar.tsx";
+import SearchBar from "@components/common/SearchBar.tsx";
+import QuestionsPreviewCard from "@components/questions/QuestionsPreviewCard.tsx";
+
+const QuestionList = () => {
+  // 더미 데이터
+  const questionLists = [
+    {
+      id: 1,
+      title: "프론트엔드 기술 면접",
+      questionCount: 25,
+      usage: 128,
+      isStarred: true,
+      category: "Frontend",
+    },
+    {
+      id: 2,
+      title: "React 심화 면접 질문",
+      questionCount: 30,
+      usage: 89,
+      isStarred: false,
+      category: "React",
+    },
+    {
+      id: 3,
+      title: "JavaScript 핵심 개념",
+      questionCount: 40,
+      usage: 156,
+      isStarred: true,
+      category: "JavaScript",
+    },
+    {
+      id: 4,
+      title: "웹 성능 최적화",
+      questionCount: 20,
+      usage: 67,
+      isStarred: false,
+      category: "Performance",
+    },
+    {
+      id: 5,
+      title: "CSS 레이아웃 마스터",
+      questionCount: 15,
+      usage: 45,
+      isStarred: false,
+      category: "CSS",
+    },
+    {
+      id: 6,
+      title: "웹 접근성과 SEO",
+      questionCount: 35,
+      usage: 92,
+      isStarred: true,
+      category: "Accessibility",
+    },
+  ];
+
+  return (
+    <section className="flex w-screen min-h-screen ">
+      <Sidebar />
+      <div className="max-w-7xl w-full mx-auto p-8">
+        <div className="mb-12">
+          <h1 className="text-4xl font-bold text-black dark:text-white my-4">
+            질문지 목록
+          </h1>
+          <div className="flex flex-wrap gap-4 items-center justify-between">
+            <SearchBar text={"질문지 검색하기"} />
+            <button className="flex items-center gap-2 px-4 py-3 bg-emerald-600 text-white rounded-lg hover:bg-emerald-500 transition-colors">
+              <FaPlus className="w-5 h-5" />새 질문지 만들기
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-6">
+          {questionLists.map((list) => (
+            <QuestionsPreviewCard
+              key={list.id}
+              id={list.id}
+              questionCount={list.questionCount}
+              category={list.category}
+              title={list.title}
+              isStarred={list.isStarred}
+              usage={list.usage}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default QuestionList;
