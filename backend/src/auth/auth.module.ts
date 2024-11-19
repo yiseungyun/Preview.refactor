d9afbb8f9eaf1888d41259e08cb8a094e5e9b652
Chanwoo Kim <kcw2205@naver.com>
2024-11-19 14:15:33 +0900

    refactor: `github.strategy.ts` 파일 위치 변경
    - `auth` 와 관련되며, 여러 `strategy` 가 생길 수 있으므로, `auth/strategy` 에 위치

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { GithubStrategy } from "../config/github.strategy";
+import { GithubStrategy } from "./strategy/github.strategy";
 import { UserRepository } from "../user/user.repository";
 
 @Module({
