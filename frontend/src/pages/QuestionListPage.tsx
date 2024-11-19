c44c76cfa654a6f7ce6fd90342a7f43cd35adbf5
JeongwooSeo <shipfriend4371@naver.com>
2024-11-19 22:23:52 +0900

    refactor: 그리드 최대 3열로 수정

--- frontend/src/pages/QuestionListPage.tsx
+++ frontend/src/pages/QuestionListPage.tsx
@@ -108,7 +108,7 @@ const QuestionList = () => {
           </div>
         </div>
         <LoadingIndicator loadingState={questionLoading} />
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  gap-6">
           {questionList.map((list) => (
             <QuestionsPreviewCard
               key={list.id}
