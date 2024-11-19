ab086a7e816dc1b0bd5359159eae93eb651be5b0
twalla26 <ssm4748@naver.com>
2024-11-20 04:05:01 +0900

    feat: access token, refresh token 발급

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,25 +1,43 @@
-import { Controller, Get, Redirect, Req, UseGuards } from "@nestjs/common";
+import { Controller, Get, Res, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
+import { Response } from "express";
+import { JwtAuthenticationGuard } from "./jwt.guard";
 
 @Controller("auth")
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
     @Get("github")
     @UseGuards(AuthGuard("github"))
-    async githubLogin(): Promise<void> {}
+    async githubAuth() {}
 
+    /* Get Google Auth Callback */
     @Get("github/login")
     @UseGuards(AuthGuard("github"))
-    @Redirect()
-    async githubLoginCallback(@Req() req) {
-        const username: string = req.user.username;
-        if (username) return { url: "/login/success/" + username };
-        return { url: "/login/failure" };
+    async githubAuthCallback(@Req() req, @Res() res: Response) {
+        try {
+            const accessToken = await this.authService.githubLogin(req, res);
+            return res.json({
+                success: true,
+                message: "login success",
+                accessToken,
+            });
+        } catch (error) {
+            return res.json({
+                success: false,
+                message: "login failed",
+                error: error.message,
+            });
+        }
     }
 
-    @Get("protected")
-    @UseGuards(AuthGuard("jwt"))
-    protectedResource() {
-        return "JWT is working!";
-
+    @Get("github/whoami")
+    @UseGuards(JwtAuthenticationGuard)
+    async whoami(@Req() req, @Res() res: Response) {
+        return res.json({
+            githubId: req.user.githubId,
+            username: req.user.username,
+        });
     }
 }
