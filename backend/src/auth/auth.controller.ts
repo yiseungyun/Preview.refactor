7dd9d48be8138f1efebeb997260b7e301472e7b6
Chanwoo Kim <kcw2205@naver.com>
2024-11-20 21:11:01 +0900

    feat: auth 도메인 수정
    - auth 컨트롤러에서 이제 `whoami` 를 통해 `accessToken` 에 접근 가능
    - `refresh` 로 접근하여 새로운 access token 발급 가능

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,40 +1,65 @@
-import { Controller, Get,Post, Res, Req, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Res, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
+import { Request, Response } from "express";
 import { AuthService } from "./auth.service";
-import { GithubProfile } from "./decorator/gitub-profile.decorator";
+import { GithubProfile } from "./github/gitub-profile.decorator";
 import { Profile } from "passport-github";
-import { Response } from "express";
-import { JwtAuthenticationGuard } from "./jwt.guard";
+import { setCookieConfig } from "../config/cookie.config";
+import { JwtPayload, JwtTokenPair } from "./jwt/jwt.decorator";
+import { IJwtPayload, IJwtTokenPair } from "./jwt/jwt.model";
 
 @Controller("auth")
 export class AuthController {
+    private static ACCESS_TOKEN = "accessToken";
+    private static REFRESH_TOKEN = "refreshToken";
+
     constructor(private readonly authService: AuthService) {}
 
     @Post("github")
     @UseGuards(AuthGuard("github"))
     async githubCallback(
         @Req() req: Request,
+        @Res({ passthrough: true }) res: Response,
         @GithubProfile() profile: Profile
     ) {
         const id = parseInt(profile.id);
 
-        // const user = await this.authService.getUserByGithubId(id);
-        // const jwtToken = await this.authService.createJwtToken(user);
+        const result = await this.authService.getTokenByGithubId(id);
+
+        res.cookie("accessToken", result.accessToken.token, {
+            maxAge: result.accessToken.expireTime,
+            ...setCookieConfig,
+        });
+
+        res.cookie("refreshToken", result.refreshToken.token, {
+            maxAge: result.refreshToken.expireTime,
+            ...setCookieConfig,
+        });
 
         return {
-            id,
             success: true,
-            // token: jwtToken,
         };
-        }
+    }
 
     @Get("whoami")
-    @UseGuards(JwtAuthenticationGuard)
-    async whoami(@Req() req, @Res() res: Response) {
-        return res.json({
-            userId: req.user.userId,
-            username: req.user.username,
+    @UseGuards(AuthGuard("jwt"))
+    async handleWhoami(@Req() req: Request, @JwtPayload() token: IJwtPayload) {
+        return token;
+    }
+
+    @Get("refresh")
+    @UseGuards(AuthGuard("jwt-refresh"))
+    async handleRefresh(
+        @Res({ passthrough: true }) res: Response,
+        @JwtTokenPair() token: IJwtTokenPair
+    ) {
+        res.cookie("accessToken", token.accessToken.token, {
+            maxAge: token.accessToken.expireTime,
+            ...setCookieConfig,
         });
+
+        return {
+            success: true,
+        };
     }
 }
