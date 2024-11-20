70c93dda65df38b439a47cb83751cafdc8c35a54
JeongwooSeo <shipfriend4371@naver.com>
2024-11-20 15:43:05 +0900

    refactor: custom passport strategy로 변경
    Co-authored-by: blu3piece <kcw2205@naver.com>

--- backend/src/auth/strategy/github.strategy.ts
+++ backend/src/auth/strategy/github.strategy.ts
@@ -1,43 +1,52 @@
-import { Injectable } from "@nestjs/common";
+// github-auth.strategy.ts
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-custom";
+import { Request } from "express";
+import axios from "axios";
 import "dotenv/config";
-import { Profile, Strategy } from "passport-github";
-import { AuthService } from "../auth.service";
 
 @Injectable()
 export class GithubStrategy extends PassportStrategy(Strategy, "github") {
-    constructor(private readonly authService: AuthService) {
-        super({
-            clientID: process.env.OAUTH_GITHUB_ID, // CLIENT_ID
-            clientSecret: process.env.OAUTH_GITHUB_SECRET, // CLIENT_SECRET
-            callbackURL: process.env.OAUTH_GITHUB_CALLBACK, // redirect_uri
-            passReqToCallback: true,
-            scope: ["profile"], // 가져올 정보들
-        });
+    constructor() {
+        super();
     }
 
-    /**
-     * GitHub에서 반환된 프로필 데이터를 가공
-     * @param request
-     * @param accessToken
-     * @param refreshToken
-     * @param profile
-     * @param done
-     */
-    async validate(
-        request: any,
-        accessToken: string,
-        refreshToken: string,
-        profile: Profile,
-        done: (error: any, user?: any) => void
-    ) {
-        try {
-            const user = await this.authService.githubLogin(profile);
-            console.log(user);
-            done(null, user);
-        } catch (err) {
-            console.error(err);
-            done(err, false);
+    async validate(req: Request, done: any) {
+        console.log(req.body);
+        const code = req.body.code;
+
+        if (!code) {
+            throw new UnauthorizedException("Authorization code not found");
         }
+
+        const tokenResponse = await axios.post(
+            "https://github.com/login/oauth/access_token",
+            {
+                client_id: process.env.OAUTH_GITHUB_ID,
+                client_secret: process.env.OAUTH_GITHUB_SECRET,
+                code: code,
+            },
+            {
+                headers: { Accept: "application/json" },
+            }
+        );
+
+        console.log("토큰 받아오기 성공");
+        const { access_token } = tokenResponse.data;
+
+        console.log(access_token);
+        // GitHub 사용자 정보 조회
+        const userResponse = await axios.get("https://api.github.com/user", {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
+        });
+
+        console.log("잘됨?", userResponse.data.id);
+
+        return done(null, {
+            profile: userResponse.data,
+        });
     }
 }
