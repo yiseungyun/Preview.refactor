8bddf473c6fb05ce949d59e99ea525548e02ec36
JeongwooSeo <shipfriend4371@naver.com>
2024-11-21 00:21:44 +0900

    refactor: 세션생성 페이지 사이드바 추가

--- frontend/src/pages/CreateSessionPage.tsx
+++ frontend/src/pages/CreateSessionPage.tsx
@@ -3,6 +3,7 @@ import SessionForm from "@/components/sessions/create/SessionForm";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import useSessionFormStore from "@/stores/useSessionFormStore";
+import Sidebar from "@components/common/Sidebar.tsx";
 
 const CreateSessionPage = () => {
   const navigate = useNavigate();
@@ -14,20 +15,23 @@ const CreateSessionPage = () => {
   }, []);
 
   return (
-    <div className="m-20">
-      <button
-        className="flex items-center gap-4 mb-5 text-gray-black text-medium-l"
-        onClick={() => navigate("/sessions")}
-      >
-        <IoArrowBackSharp className="w-5 h-5" />
-        <span>면접 채널로 돌아가기</span>
-      </button>
-      <h1 className="text-bold-l mb-2">새로운 스터디 세션 만들기</h1>
-      <p className="text-medium-l text-gray-400 mb-8">
-        면접 스터디를 위한 새로운 세션을 생성합니다.
-      </p>
-      <SessionForm />
-    </div>
+    <section className="flex w-screen h-screen">
+      <Sidebar />
+      <div className={"p-20"}>
+        <button
+          className="flex items-center gap-4 mb-5 text-gray-black text-medium-l"
+          onClick={() => navigate("/sessions")}
+        >
+          <IoArrowBackSharp className="w-5 h-5" />
+          <span>면접 채널로 돌아가기</span>
+        </button>
+        <h1 className="text-bold-l mb-2">새로운 스터디 세션 만들기</h1>
+        <p className="text-medium-l text-gray-400 mb-8">
+          면접 스터디를 위한 새로운 세션을 생성합니다.
+        </p>
+        <SessionForm />
+      </div>
+    </section>
   );
 };
 
