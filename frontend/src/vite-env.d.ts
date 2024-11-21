e91e14dc1483d28b03901887d4003f98940c09dc
JeongwooSeo <shipfriend4371@naver.com>
2024-11-21 18:42:46 +0900

    feat: vite 환경변수 타입 설정 및 vite.config.ts에서도 환경변수 사용할 수 있도록 구현

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/types/importMeta.d.ts" />
+
+interface ImportMetaEnv {
+  readonly [key: string]: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
