d2b1e174dec91cd3087146ea1cd26fd16a46d7f8
yiseungyun <seungxxyun@gmail.com>
2024-11-21 11:08:14 +0900

    feat: 질문지 상세 페이지 생성

---
+++ frontend/src/pages/QuestionDetailPage.tsx
@@ -0,0 +1,12 @@
+//import { useParams } from "react-router-dom";
+
+const QuestionDetailPage = () => {
+  //const { questionId } = useParams();
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default QuestionDetailPage;
