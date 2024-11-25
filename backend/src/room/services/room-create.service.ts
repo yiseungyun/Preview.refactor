5477d1e561e0f45d07439396429de7a1ed4cf412
Chanwoo Kim <kcw2205@naver.com>
2024-11-25 20:55:58 +0900

    refactor: 스터디 세션 입장, 생성 로직 분리
    - 길어지는 로직을 아예 파일로 분리했음
    - 현재로써는 로직의 수가 많지 않기에 가독성 측면에서 분리가 좋다고 판단했습니다.

---
+++ backend/src/room/services/room-create.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from "@nestjs/common";
+import { CreateRoomInternalDto } from "@/room/dto/create-room.dto";
+import { EMIT_EVENT } from "@/room/room.events";
+import { WebsocketService } from "@/websocket/websocket.service";
+import { RoomRepository } from "@/room/room.repository";
+import { QuestionListRepository } from "@/question-list/question-list.repository";
+import { RoomJoinService } from "@/room/services/room-join.service";
+import { createHash } from "node:crypto";
+import "dotenv/config";
+
+@Injectable()
+export class RoomCreateService {
+    private static ROOM_ID_CREATE_KEY = "room_id";
+
+    public constructor(
+        private readonly roomRepository: RoomRepository,
+        private readonly socketService: WebsocketService,
+        private readonly questionListRepository: QuestionListRepository,
+        private readonly roomJoinService: RoomJoinService
+    ) {}
+
+    public async createRoom(dto: CreateRoomInternalDto) {
+        const { socketId, nickname } = dto;
+        const roomId = await this.generateRoomId();
+        const currentTime = Date.now();
+        const questionListContents = await this.questionListRepository.getContentsByQuestionListId(
+            dto.questionListId
+        );
+
+        const roomDto = {
+            ...dto,
+            roomId,
+            connectionList: [],
+            questionListContents,
+            createdAt: currentTime,
+            host: dto.socketId,
+        };
+
+        await this.roomRepository.setRoom(roomDto);
+
+        await this.roomJoinService.joinRoom({ roomId, socketId, nickname });
+
+        this.socketService.emitToRoom(roomId, EMIT_EVENT.CREATE, roomDto);
+    }
+
+    // TODO: 동시성 고려해봐야하지 않을까?
+    private async generateRoomId() {
+        const client = this.socketService.getRedisClient();
+
+        const idString = await client.get(RoomCreateService.ROOM_ID_CREATE_KEY);
+
+        let id: number;
+        if (idString && !isNaN(parseInt(idString))) {
+            id = await client.incr(RoomCreateService.ROOM_ID_CREATE_KEY);
+        } else {
+            id = parseInt(await client.set(RoomCreateService.ROOM_ID_CREATE_KEY, "1"));
+        }
+
+        return createHash("sha256")
+            .update(id + process.env.SESSION_HASH)
+            .digest("hex");
+    }
+}
