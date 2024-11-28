687c092927adbd7202a12396fe5eaee87c49c989
JeongwooSeo <shipfriend4371@naver.com>
2024-11-28 16:35:56 +0900

    feat: 스터디가 진행 중인지 아닌지를 직관적으로 확인할 수 있게 구현

--- frontend/src/components/session/SessionHeader.tsx
+++ frontend/src/components/session/SessionHeader.tsx
@@ -1,4 +1,5 @@
 import { RoomMetadata } from "@hooks/type/session";
+import { useEffect, useState } from "react";
 
 interface SessionHeaderProps {
   roomMetadata: RoomMetadata | null;
@@ -9,6 +10,19 @@ const SessionHeader = ({
   participantsCount,
   roomMetadata,
 }: SessionHeaderProps) => {
+  const [uptime, setUptime] = useState<number>(0);
+
+  useEffect(() => {
+    if (!roomMetadata?.inProgress) return;
+    const interval = setInterval(() => {
+      setUptime((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [roomMetadata?.inProgress]);
+
   return (
     <div
       className={
@@ -26,6 +40,19 @@ const SessionHeader = ({
             {roomMetadata &&
               `(${participantsCount} / ${roomMetadata.maxParticipants})`}
           </span>
+          {roomMetadata.inProgress ? (
+            <span>
+              <span
+                className={"w-3 h-3 bg-red-700 animate-pulse shadow-red-500"}
+              ></span>
+              스터디 진행 중 {uptime}초
+            </span>
+          ) : (
+            <span>
+              <span className={"w-3 h-3 bg-gray-300  "}></span>
+              스터디 시작 전
+            </span>
+          )}
         </>
       ) : (
         <h1>아직 세션에 참가하지 않았습니다.</h1>
