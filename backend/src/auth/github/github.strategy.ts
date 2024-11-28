c961ea82b6f86ba4996410a48a2b32bb977e1e71
Chanwoo Kim <kcw2205@naver.com>
2024-11-28 14:55:51 +0900

    feat: 깃허브 전략 수정
    - `인증`까지만 범위 설정을 어떻게 할지 고민을 많이 했음
    - 인증에 성공했다면, 토큰을 받아오도록 통일해야하지 않을까? 라는 생각을 하게 됨
    - 그렇게 전략 패턴별로 토큰을 반환하도록 통일함
    - GithubProfile 이라는 정보도 나중에 삭제할 예정

--- backend/src/auth/github/github.strategy.ts
+++ backend/src/auth/github/github.strategy.ts
@@ -5,47 +5,59 @@ import { Strategy } from "passport-custom";
 import { Request } from "express";
 import axios from "axios";
 import "dotenv/config";
+import { AuthService } from "@/auth/auth.service";
+import { JwtService } from "@/auth/jwt/jwt.service";
 
 @Injectable()
 export class GithubStrategy extends PassportStrategy(Strategy, "github") {
-    private static REQUEST_ACCESS_TOKEN_URL =
-        "https://github.com/login/oauth/access_token";
+    private static REQUEST_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
     private static REQUEST_USER_URL = "https://api.github.com/user";
 
-    constructor() {
+    constructor(
+        private readonly authService: AuthService,
+        private readonly jwtService: JwtService
+    ) {
         super();
     }
 
-    async validate(req: Request, done: any) {
+    async validate(req: Request) {
         const { code } = req.body;
 
         if (!code) {
             throw new UnauthorizedException("Authorization code not found");
         }
 
-        const tokenResponse = await axios.post(
+        const { access_token: accessToken } = (await this.fetchAccessToken(code)).data;
+
+        const profile = (await this.fetchGithubUser(accessToken)).data;
+
+        const user = await this.authService.getUserByGithubId(profile.id);
+        const token = await this.jwtService.createJwtToken(user);
+
+        return {
+            jwtToken: token,
+        };
+    }
+
+    private async fetchAccessToken(code: string) {
+        return axios.post(
             GithubStrategy.REQUEST_ACCESS_TOKEN_URL,
             {
                 client_id: process.env.OAUTH_GITHUB_ID,
                 client_secret: process.env.OAUTH_GITHUB_SECRET,
-                code: code,
+                code,
             },
             {
                 headers: { Accept: "application/json" },
             }
         );
+    }
 
-        const { access_token } = tokenResponse.data;
-
-        // GitHub 사용자 정보 조회
-        const userResponse = await axios.get(GithubStrategy.REQUEST_USER_URL, {
+    private async fetchGithubUser(accessToken: string) {
+        return axios.get(GithubStrategy.REQUEST_USER_URL, {
             headers: {
-                Authorization: `Bearer ${access_token}`,
+                Authorization: `Bearer ${accessToken}`,
             },
-        });
-
-        return done(null, {
-            profile: userResponse.data,
         });
     }
 }
