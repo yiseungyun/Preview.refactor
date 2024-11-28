47007e01a9f8b739cf451987207ad87d9a177e7e
Chanwoo Kim <kcw2205@naver.com>
2024-11-28 14:58:20 +0900

    feat: 바뀐 명세에 따른 모듈 의존성 업데이트

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { GithubStrategy } from "./github/github.strategy";
-import { UserRepository } from "../user/user.repository";
+import { UserRepository } from "@/user/user.repository";
 import { JwtModule } from "./jwt/jwt.module";
+import { LocalStrategy } from "@/auth/local/local.strategy";
+import { UserService } from "@/user/user.service";
 
 @Module({
     imports: [JwtModule],
     controllers: [AuthController],
-    providers: [AuthService, GithubStrategy, UserRepository],
+    providers: [AuthService, GithubStrategy, LocalStrategy, UserRepository, UserService],
 })
 export class AuthModule {}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
+import { UserService } from "@/user/user.service";
+import { AuthService } from "@/auth/auth.service";
+import { JwtModule } from "@/auth/jwt/jwt.module";
+import { UserController } from "@/user/user.controller";
 
 @Module({
-    providers: [UserRepository],
+    imports: [JwtModule],
+    controllers: [UserController],
+    providers: [UserRepository, UserService, AuthService],
 })
 export class UserModule {}
