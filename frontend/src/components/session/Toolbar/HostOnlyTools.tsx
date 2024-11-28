dde42e08f0761a25f00f3cd97816ca1d1432e11c
JeongwooSeo <shipfriend4371@naver.com>
2024-11-28 21:06:58 +0900

    feat: 다음 질문, 이전 질문 누르고 2초 후에 활성화 되도록 구현

--- frontend/src/components/session/Toolbar/HostOnlyTools.tsx
+++ frontend/src/components/session/Toolbar/HostOnlyTools.tsx
@@ -1,4 +1,5 @@
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 
 // 툴바에서 호스트만 사용가능 도구들 분리
 interface HostOnlyToolsProps {
@@ -15,6 +16,21 @@ const HostOnlyTools = ({
   startStudySession,
   requestChangeIndex,
 }: HostOnlyToolsProps) => {
+  const [changeCooldown, setChangeCooldown] = useState(false);
+  const COOLDOWN_TIME = 2000;
+
+  useEffect(() => {
+    if (!changeCooldown) return;
+
+    const timeout = setTimeout(() => {
+      setChangeCooldown(false);
+    }, COOLDOWN_TIME);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [changeCooldown]);
+
   return (
     isHost && (
       <>
@@ -24,7 +40,9 @@ const HostOnlyTools = ({
               className={
                 "bg-transparent rounded-xl border h-10 px-3 py-2 text-medium-xs "
               }
-              onClick={stopStudySession}
+              onClick={() => {
+                stopStudySession();
+              }}
             >
               스터디 종료하기
             </button>
@@ -35,7 +53,9 @@ const HostOnlyTools = ({
               className={
                 "bg-transparent rounded-xl border h-10 px-3 py-2 text-medium-xs "
               }
-              onClick={startStudySession}
+              onClick={() => {
+                startStudySession();
+              }}
             >
               스터디 시작하기
             </button>
@@ -44,22 +64,44 @@ const HostOnlyTools = ({
         {isInProgress && (
           <div className={"study-toolbar inline-flex gap-4 items-center mx-8"}>
             <button
-              onClick={() => requestChangeIndex("prev")}
+              onClick={() => {
+                requestChangeIndex("prev");
+                setChangeCooldown(true);
+              }}
               className={
-                "inline-flex items-center bg-transparent rounded-xl border h-10 px-3 py-2 text-medium-xs "
+                "relative inline-flex items-center bg-transparent rounded-xl border h-10 px-3 py-2 text-medium-xs disabled:opacity-50"
               }
               aria-label={"이전 질문 버튼"}
+              disabled={changeCooldown}
             >
               <FaAngleLeft /> 이전 질문
+              {changeCooldown && (
+                <div
+                  className={
+                    "origin-left absolute w-full h-full bg-gray-400/50 top-0 left-0 animate-progress"
+                  }
+                ></div>
+              )}
             </button>
             <button
-              onClick={() => requestChangeIndex("next")}
+              onClick={() => {
+                requestChangeIndex("next");
+                setChangeCooldown(true);
+              }}
               className={
-                "inline-flex items-center bg-transparent rounded-xl border h-10 px-3 py-2 text-medium-xs "
+                "relative inline-flex items-center bg-transparent rounded-xl border h-10 px-3 py-2 text-medium-xs disabled:opacity-50 overflow-hidden"
               }
               aria-label={"다음 질문 버튼"}
+              disabled={changeCooldown}
             >
               다음 질문 <FaAngleRight />
+              {changeCooldown && (
+                <div
+                  className={
+                    "origin-left absolute w-full h-full bg-gray-400/50 top-0 left-0 animate-progress"
+                  }
+                ></div>
+              )}
             </button>
           </div>
         )}
