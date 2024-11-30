a1a2bb6d7295e1d14814a6a073a6fcf1ad2aa9f7
JeongwooSeo <shipfriend4371@naver.com>
2024-11-30 21:20:47 +0900

    style: 상세보기 페이지 다크모드 지원

--- frontend/src/components/questions/detail/QuestionList.tsx/QuestionItem.tsx
+++ frontend/src/components/questions/detail/QuestionList.tsx/QuestionItem.tsx
@@ -6,7 +6,7 @@ interface QuestionItemProps {
 const QuestionItem = ({ index, content }: QuestionItemProps) => {
   return (
     <div className="flex gap-1 border-custom-s border-gray-200 rounded-custom-m p-3">
-      <h3 className="text-semibold-m text-gray-black">Q{index + 1}.</h3>
+      <h3 className="text-semibold-m">Q{index + 1}.</h3>
       <p className="text-medium-l">{content}</p>
     </div>
   );
--- frontend/src/components/questions/detail/QuestionTitle.tsx/index.tsx
+++ frontend/src/components/questions/detail/QuestionTitle.tsx/index.tsx
@@ -25,17 +25,17 @@ const QuestionTitle = ({ questionId }: { questionId: string }) => {
     <div className="mb-4 flex flex-col gap-2">
       <div className="flex justify-between">
         <h3 className="text-semibold-l">{question.title}</h3>
-        <div className="flex gap-3 text-gray-500">
+        <div className="flex gap-3 text-gray-500 dark:text-gray-200">
           <button>
-            <MdEdit className="w-6 h-6 hover:text-gray-black" />
+            <MdEdit className="w-6 h-6 hover:text-gray-black dark:hover:text-white" />
           </button>
           <button>
-            <RiDeleteBin6Fill className="w-6 h-6 hover:text-gray-black" />
+            <RiDeleteBin6Fill className="w-6 h-6 hover:text-gray-black dark:hover:text-white" />
           </button>
         </div>
       </div>
       <div className="flex gap-3">
-        <div className="inline-flex items-center gap-1 text-medium-m text-gray-400">
+        <div className="inline-flex items-center gap-1 text-medium-m text-gray-400 dark:text-gray-200">
           <FaRegUser />
           작성자 {question.username} • {question.contents.length}개의 질문
         </div>
