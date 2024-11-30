4c870c27cddd7a7f708205438e2377d6619edfc9
JeongwooSeo <shipfriend4371@naver.com>
2024-11-30 19:59:30 +0900

    style: 질문 아이템 flex direction col -> row로 변경

--- frontend/src/components/questions/detail/QuestionList.tsx/QuestionItem.tsx
+++ frontend/src/components/questions/detail/QuestionList.tsx/QuestionItem.tsx
@@ -5,8 +5,8 @@ interface QuestionItemProps {
 
 const QuestionItem = ({ index, content }: QuestionItemProps) => {
   return (
-    <div className="flex flex-col gap-1 border-custom-s border-gray-200 rounded-custom-m p-3">
-      <h3 className="text-semibold-m text-gray-black">Q{index + 1}</h3>
+    <div className="flex gap-1 border-custom-s border-gray-200 rounded-custom-m p-3">
+      <h3 className="text-semibold-m text-gray-black">Q{index + 1}.</h3>
       <p className="text-medium-l">{content}</p>
     </div>
   );
