8f491271fd4dda2203ead5a73860ad677513d7cb
JeongwooSeo <shipfriend4371@naver.com>
2024-12-02 19:50:19 +0900

    refactor: 인트로카드 컴포넌트로 분리

--- frontend/src/pages/IntroPage/IntroPage.tsx
+++ frontend/src/pages/IntroPage/IntroPage.tsx
@@ -1,11 +1,11 @@
 import SidebarPageLayout from "@components/layout/SidebarPageLayout";
-import { FaArrowRight } from "react-icons/fa6";
 import questionList from "/introduce/questionList.png";
 import createSession from "/introduce/createSession.png";
 import inSession from "/introduce/inSession.png";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import { throttle } from "lodash";
+import IntroCard from "@/pages/IntroPage/view/IntroCard.tsx";
 
 const IntroPage = () => {
   const navigate = useNavigate();
@@ -18,7 +18,6 @@ const IntroPage = () => {
       .scrollTop;
     if (scrollTop && scrollTop > 10) {
       setIsScrolled(true);
-      console.log(1);
     } else {
       setIsScrolled(false);
     }
@@ -53,105 +52,75 @@ const IntroPage = () => {
         </div>
 
         <div className="py-32 px-8">
-          <div className="max-w-6xl mx-auto grid grid-cols-2 gap-16 items-center">
-            <div>
-              <div className="select-none text-[200px] font-bold text-gray-800">
-                01
-              </div>
-              <h2 className="text-4xl font-bold text-white mb-6">
+          <IntroCard
+            index={"01"}
+            title={
+              <>
                 나만의 맞춤형
                 <br />
                 질문 리스트 만들기
-              </h2>
-              <p className="text-gray-200 mb-6">
+              </>
+            }
+            description={
+              <>
                 막연한 면접 준비는 이제 그만! <br />
                 면접관이 자주 묻는 핵심 질문들을 모아 나만의 리스트를
                 만들어보세요.
-              </p>
-              <button
-                onClick={() => navigate("/questions/create")}
-                className="flex items-center text-green-100 hover:text-green-200 transition-colors"
-              >
-                <span className="mr-2">질문 리스트 만들기</span>
-                <FaArrowRight className="w-4 h-4" />
-              </button>
-            </div>
-            <div>
-              <img
-                src={questionList}
-                alt="질문 리스트 만들기"
-                className="w-full rounded-2xl shadow-lg"
-              />
-            </div>
-          </div>
+              </>
+            }
+            image={questionList}
+            buttonText={"질문 리스트 만들기"}
+            onClick={() => navigate("/questions/create")}
+            imagePosition={"right"}
+          />
         </div>
 
         <div className="py-32 px-8 bg-gray-900/50">
-          <div className="max-w-6xl mx-auto grid grid-cols-2 gap-16 items-center">
-            <div>
-              <img
-                src={createSession}
-                alt="스터디 세션 만들기"
-                className="w-full rounded-2xl shadow-lg"
-              />
-            </div>
-            <div>
-              <div className="select-none text-[200px] font-bold text-gray-800">
-                02
-              </div>
-              <h2 className="text-4xl font-bold text-white mb-6">
+          <IntroCard
+            index={"02"}
+            title={
+              <>
                 함께할 스터디원과
                 <br />
                 스터디 채널 개설하기
-              </h2>
-              <p className="text-gray-200 mb-6">
+              </>
+            }
+            description={
+              <>
                 준비한 질문들로 스터디 채널을 만들고 함께 성장할 동료를
                 찾아보세요. 공개 스터디로 다양한 인사이트를 얻거나, 비공개
                 스터디로 친구들과 함께 연습할 수 있습니다.
-              </p>
-              <button
-                onClick={() => navigate("/sessions/create")}
-                className="flex items-center text-green-100 hover:text-green-200 transition-colors"
-              >
-                <span className="mr-2">스터디룸 만들기</span>
-                <FaArrowRight className="w-4 h-4" />
-              </button>
-            </div>
-          </div>
+              </>
+            }
+            image={createSession}
+            buttonText={"스터디 세션 만들기"}
+            onClick={() => navigate("/sessions/create")}
+            imagePosition={"left"}
+          />
         </div>
 
         <div className="py-32 px-8">
-          <div className="max-w-6xl mx-auto grid grid-cols-2 gap-16 items-center">
-            <div>
-              <div className="select-none text-[200px] font-bold text-gray-800">
-                03
-              </div>
-              <h2 className="text-4xl font-bold text-white mb-6">
+          <IntroCard
+            index={"03"}
+            title={
+              <>
                 실전 같은 환경에서
                 <br />
                 면접 연습하기
-              </h2>
-              <p className="text-gray-200 mb-6">
+              </>
+            }
+            description={
+              <>
                 실시간 화상 면접으로 실제 면접의 긴장감을 경험해보세요. 면접관과
                 지원자 역할을 번갈아가며 연습하고, 서로의 피드백으로 더 나은
                 답변을 준비할 수 있습니다.
-              </p>
-              <button
-                onClick={() => navigate("/sessions")}
-                className="flex items-center text-green-100 hover:text-green-200 transition-colors"
-              >
-                <span className="mr-2">면접 연습 시작하기</span>
-                <FaArrowRight className="w-4 h-4" />
-              </button>
-            </div>
-            <div>
-              <img
-                src={inSession}
-                alt="면접 연습하기"
-                className="w-full rounded-2xl shadow-lg"
-              />
-            </div>
-          </div>
+              </>
+            }
+            image={inSession}
+            buttonText={"면접 연습하기"}
+            onClick={() => navigate("/sessions")}
+            imagePosition={"right"}
+          />
         </div>
 
         <div className="py-32 px-8 bg-gray-900/50 text-center">
