1440ad63c59cb96db046198dfc6ca69fda5734c7
JeongwooSeo <shipfriend4371@naver.com>
2024-12-02 21:51:27 +0900

    refactor: animation 객체 분리

--- frontend/src/pages/IntroPage/hooks/useObserver.ts
+++ frontend/src/pages/IntroPage/hooks/useObserver.ts
@@ -7,22 +7,23 @@ const useObserver = (option?: {
 }) => {
   const observer = useRef<IntersectionObserver | null>(null);
 
+  const keyframes = [
+    { opacity: 0, transform: "translateY(-50px)" },
+    { opacity: 1, transform: "translateY(0)" },
+  ];
+
+  const animationOption: KeyframeAnimationOptions = {
+    duration: 700,
+    easing: "ease-in-out",
+    fill: "both",
+  };
+
   useEffect(() => {
     observer.current = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.animate(
-              [
-                { opacity: 0, transform: "translateY(-50px)" },
-                { opacity: 1, transform: "translateY(0)" },
-              ],
-              {
-                duration: 700,
-                easing: "ease-in-out",
-                fill: "both",
-              }
-            );
+            entry.target.animate(keyframes, animationOption);
             observer.current?.unobserve(entry.target);
           }
         });
