bcf201596cb79b8c64a5628708b2bc59fea25a09
JeongwooSeo <shipfriend4371@naver.com>
2024-12-03 13:01:54 +0900

    feat: 질문지 리스트 페이지 페이지네이션 구현

--- frontend/src/pages/QuestionListPage/QuestionListPage.tsx
+++ frontend/src/pages/QuestionListPage/QuestionListPage.tsx
@@ -1,4 +1,3 @@
-import Sidebar from "@/components/common/Sidebar/Sidebar";
 import SearchBar from "@/components/common/Input/SearchBar";
 import CategorySelect from "@/components/common/Select/CategorySelect";
 import LoadingIndicator from "@components/common/LoadingIndicator.tsx";
@@ -9,19 +8,23 @@ import { useQuestionList } from "@hooks/api/useGetQuestionList.ts";
 import ErrorBlock from "@components/common/Error/ErrorBlock.tsx";
 import QuestionsPreviewList from "@/pages/QuestionListPage/view/QuestionsPreviewList.tsx";
 import useCategory from "@/pages/QuestionListPage/hooks/useCategory.ts";
+import Pagination from "@components/common/Pagination";
+import { useState } from "react";
+import SidebarPageLayout from "@components/layout/SidebarPageLayout.tsx";
 
 const QuestionListPage = () => {
   const { selectedCategory, setSelectedCategory } = useCategory();
+  const [page, setPage] = useState(1);
   const {
-    data: questionList,
+    data,
     error,
     isLoading: questionLoading,
-  } = useQuestionList({ category: selectedCategory, limit: 10 });
+  } = useQuestionList({ category: selectedCategory, page: page, limit: 12 });
 
+  console.log(data);
   return (
-    <section className="flex w-screen min-h-screen">
-      <Sidebar />
-      <div className="max-w-6xl w-full px-12 pt-20">
+    <SidebarPageLayout>
+      <div className="max-w-6xl flex flex-col w-full px-12 pt-20  h-full">
         <div className="mb-12">
           <h1 className="text-bold-l text-gray-black dark:text-white mb-6">
             질문지 리스트
@@ -40,17 +43,26 @@ const QuestionListPage = () => {
             />
           </div>
         </div>
-        <LoadingIndicator loadingState={questionLoading} />
-        <QuestionsPreviewList
-          questionList={questionList}
-          questionLoading={questionLoading}
-        />
-        <ErrorBlock
-          error={error}
-          message={"질문지 목록을 불러오는데 실패했습니다!"}
-        />
+        <div className={"flex flex-col justify-between flex-grow"}>
+          <LoadingIndicator loadingState={questionLoading} />
+          <QuestionsPreviewList
+            questionList={data?.allQuestionLists}
+            questionLoading={questionLoading}
+          />
+          <div className={"mb-20 mt-10"}>
+            <Pagination
+              currentPage={page}
+              totalPage={data?.meta.totalPages || 0}
+              onPageChange={setPage}
+            />
+          </div>
+          <ErrorBlock
+            error={error}
+            message={"질문지 목록을 불러오는데 실패했습니다!"}
+          />
+        </div>
       </div>
-    </section>
+    </SidebarPageLayout>
   );
 };
 
