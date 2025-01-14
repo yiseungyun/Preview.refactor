f42ce0c1b13bb1c2fe7283573ddc212ff75e6027
yiseungyun <seungxxyun@gmail.com>
2025-01-14 21:44:06 +0900

    refactor: UI 내 조건부 렌더링 외부에서 처리하도록 수정

--- frontend/src/components/session/VideoProfileOverlay.tsx
+++ frontend/src/components/session/VideoProfileOverlay.tsx
@@ -11,31 +11,28 @@ const VideoProfileOverlay = ({
   nickname,
   profileImage,
 }: VideoProfileOverlayProps) => {
+  if (isVideoOn || videoLoading) {
+    return null;
+  }
+
   return (
-    !isVideoOn &&
-    !videoLoading && (
-      <div
-        className={
-          "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-300/20 text-white  p-2 rounded-xl text-3xl overflow-hidden flex justify-center items-center z-10"
-        }
-      >
-        {profileImage ? (
-          <div
-            className={
-              "aspect-1 w-2/3 h-2/3 bg-gray-400 rounded-full overflow-hidden"
-            }
-          >
-            <img
-              className={"object-cover h-full opacity-100"}
-              src={profileImage}
-              alt={nickname + "프로필 이미지"}
-            />
-          </div>
-        ) : (
-          <span className={"w-full text-center"}>{nickname}</span>
-        )}
-      </div>
-    )
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-300/20 text-white  p-2 rounded-xl text-3xl overflow-hidden flex justify-center items-center z-10">
+      {profileImage ? (
+        <div
+          className={
+            "aspect-1 w-2/3 h-2/3 bg-gray-400 rounded-full overflow-hidden"
+          }
+        >
+          <img
+            className={"object-cover h-full opacity-100"}
+            src={profileImage}
+            alt={nickname + "프로필 이미지"}
+          />
+        </div>
+      ) : (
+        <span className={"w-full text-center"}>{nickname}</span>
+      )}
+    </div>
   );
 };
 export default VideoProfileOverlay;
