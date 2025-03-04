268d1987471c1f6a86c38f7093c80bc3020fe879
yiseungyun <seungxxyun@gmail.com>
2025-03-04 10:55:48 +0900

    refactor: 채널 리스트 목록 Link가 아닌 li로 구성되도록 변경

--- frontend/src/components/channel/CommonTools.tsx
+++ frontend/src/components/channel/CommonTools.tsx
@@ -46,7 +46,7 @@ const CommonTools = ({ socket, disconnect }: CommonToolsProps) => {
 
   const existHandler = () => {
     socket.emit(SESSION_EMIT_EVENT.LEAVE, { roomId });
-    toast.success("메인 화면으로 이동합니다.");
+    toast.success("채널 목록으로 이동합니다.");
     setShouldBlock(false);
     navigate("/channels");
   };
@@ -54,7 +54,7 @@ const CommonTools = ({ socket, disconnect }: CommonToolsProps) => {
   const destroyAndExitHandler = () => {
     socket.off(SESSION_EMIT_EVENT.FINISH);
     socket.emit(SESSION_EMIT_EVENT.FINISH, { roomId });
-    toast.success("메인 화면으로 이동합니다.");
+    toast.success("채널 목록으로 이동합니다.");
     setShouldBlock(false);
     navigate("/channels");
   };
@@ -67,7 +67,7 @@ const CommonTools = ({ socket, disconnect }: CommonToolsProps) => {
   const modalProps = isHost
     ? {
       title: "세션을 종료할까요?",
-      subtitle: "호스트 권한을 주지 않으면 방이 종료됩니다.",
+      subtitle: "호스트 권한을 주지 않으면 채널이 종료됩니다.",
       leftButton: "호스트 권한 전달",
       rightButton: "종료하기",
       type: "red" as const,
@@ -85,92 +85,90 @@ const CommonTools = ({ socket, disconnect }: CommonToolsProps) => {
     };
 
   return (
-    <>
-      <div className="inline-flex gap-2">
-        <ToolTip text="세션 링크 복사">
-          <button
-            className="aspect-square px-3 bg-green-200 text-white rounded-custom-m border-custom-s border-green-200 hover:bg-green-100"
-            aria-label="세션 링크 복사"
-            onClick={shareSessionLink}
-          >
-            <MdLink className="w-6 h-6" />
-          </button>
-        </ToolTip>
-        <div className="relative">
-          <select
-            className="w-16 bg-white text-white py-3 rounded-custom-m border-custom-s border-gray-200"
-            onChange={(e) => setSelectedAudioDeviceId(e.target.value)}
-          >
-            {userAudioDevices.length > 0 ? (
-              userAudioDevices.map((device) => (
-                <option key={device.deviceId} value={device.deviceId}>
-                  {device.label}
-                </option>
-              ))
-            ) : (
-              <option value={""}>발견된 오디오 장치가 없습니다.</option>
-            )}
-          </select>
-          <button
-            onClick={handleMicClick}
-            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
-            aria-label={isMicOn ? "마이크 끄기" : "마이크 켜기"}
-          >
-            {isMicOn
-              ? <MdMic className="w-6 h-6 hover:text-green-500" />
-              : <MdMicOff className="w-6 h-6 text-point-1 hover:text-green-500" />
-            }
-          </button>
-          <span className="absolute top-1/2 -translate-y-1/2 right-2 pointer-events-none">
-            <IoChevronDownSharp className="w-3.5 h-3.5 text-gray-500" />
-          </span>
-        </div>
-        <div className="relative">
-          <select
-            className="w-16 bg-white text-white py-3 rounded-custom-m border-custom-s border-gray-200"
-            onChange={(e) => setSelectedVideoDeviceId(e.target.value)}
-          >
-            {userVideoDevices.length > 0 ? (
-              userVideoDevices.map((device) => (
-                <option key={device.deviceId} value={device.deviceId}>
-                  {device.label}
-                </option>
-              ))
-            ) : (
-              <option value={""}>발견된 비디오 장치가 없습니다.</option>
-            )}
-          </select>
-          <button
-            disabled={videoLoading}
-            onClick={handleVideoClick}
-            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
-            aria-label={isVideoOn ? "비디오 끄기" : "비디오 켜기"}
-          >
-            {isVideoOn
-              ? <MdVideocam className="w-6 h-6 hover:text-green-500" />
-              : <MdVideocamOff className="w-6 h-6 text-point-1 hover:text-green-500" />
-            }
-          </button>
-          <span className="absolute top-1/2 -translate-y-1/2 right-2 pointer-events-none">
-            <IoChevronDownSharp className="w-3.5 h-3.5 text-gray-500" />
-          </span>
-        </div>
+    <div className="inline-flex gap-2">
+      <ToolTip text="세션 링크 복사">
         <button
-          onClick={() => emitReaction("thumbs_up")}
-          className="aspect-square px-3 bg-white text-gray-600 rounded-custom-m border-custom-s border-gray-200"
-          aria-label={"좋아요"}
+          className="aspect-square px-3 bg-green-200 text-white rounded-custom-m border-custom-s border-green-200 hover:bg-green-100"
+          aria-label="세션 링크 복사"
+          onClick={shareSessionLink}
         >
-          <MdThumbUp className="w-5 h-5 hover:text-green-500" />
+          <MdLink className="w-6 h-6" />
         </button>
+      </ToolTip>
+      <div className="relative">
+        <select
+          className="w-16 bg-white text-white py-3 rounded-custom-m border-custom-s border-gray-200"
+          onChange={(e) => setSelectedAudioDeviceId(e.target.value)}
+        >
+          {userAudioDevices.length > 0 ? (
+            userAudioDevices.map((device) => (
+              <option key={device.deviceId} value={device.deviceId}>
+                {device.label}
+              </option>
+            ))
+          ) : (
+            <option value={""}>발견된 오디오 장치가 없습니다.</option>
+          )}
+        </select>
         <button
-          className="bg-point-1 rounded-custom-m border-custom-s border-gray-200 text-white text-semibold-r px-4"
-          onClick={modal.openModal}
+          onClick={handleMicClick}
+          className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+          aria-label={isMicOn ? "마이크 끄기" : "마이크 켜기"}
         >
-          종료하기
+          {isMicOn
+            ? <MdMic className="w-6 h-6 hover:text-green-500" />
+            : <MdMicOff className="w-6 h-6 text-point-1 hover:text-green-500" />
+          }
         </button>
-        <Modal modal={modal} {...modalProps} />
+        <span className="absolute top-1/2 -translate-y-1/2 right-2 pointer-events-none">
+          <IoChevronDownSharp className="w-3.5 h-3.5 text-gray-500" />
+        </span>
       </div>
-    </>
+      <div className="relative">
+        <select
+          className="w-16 bg-white text-white py-3 rounded-custom-m border-custom-s border-gray-200"
+          onChange={(e) => setSelectedVideoDeviceId(e.target.value)}
+        >
+          {userVideoDevices.length > 0 ? (
+            userVideoDevices.map((device) => (
+              <option key={device.deviceId} value={device.deviceId}>
+                {device.label}
+              </option>
+            ))
+          ) : (
+            <option value="">발견된 비디오 장치가 없습니다.</option>
+          )}
+        </select>
+        <button
+          disabled={videoLoading}
+          onClick={handleVideoClick}
+          className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+          aria-label={isVideoOn ? "비디오 끄기" : "비디오 켜기"}
+        >
+          {isVideoOn
+            ? <MdVideocam className="w-6 h-6 hover:text-green-500" />
+            : <MdVideocamOff className="w-6 h-6 text-point-1 hover:text-green-500" />
+          }
+        </button>
+        <span className="absolute top-1/2 -translate-y-1/2 right-2 pointer-events-none">
+          <IoChevronDownSharp className="w-3.5 h-3.5 text-gray-500" />
+        </span>
+      </div>
+      <button
+        onClick={() => emitReaction("thumbs_up")}
+        className="aspect-square px-3 bg-white text-gray-600 rounded-custom-m border-custom-s border-gray-200"
+        aria-label="좋아요"
+      >
+        <MdThumbUp className="w-5 h-5 hover:text-green-500" />
+      </button>
+      <button
+        className="bg-point-1 rounded-custom-m border-custom-s border-gray-200 text-white text-semibold-r px-4"
+        onClick={modal.openModal}
+      >
+        종료하기
+      </button>
+      <Modal modal={modal} {...modalProps} />
+    </div>
   );
 };
 
--- frontend/src/pages/channels/ChannelCard.tsx
+++ frontend/src/pages/channels/ChannelCard.tsx
@@ -19,33 +19,35 @@ const ChannelCard = ({
   onEnter,
 }: ChannelCardProps) => {
   return (
-    <Link
-      onClick={onEnter}
-      to={`/channel/${id}`}
-      className={`relative h-52 bg-white rounded-custom-m px-5 py-6 transition-all duration-200 ease-in-out hover:-translate-y-1.5 border-custom-s border-gray-200
-        ${inProgress === false ? "hover:shadow-16 hover:ring-1 hover:ring-green-200" : ""}`}
-    >
-      <div className="flex flex-col justify-between">
-        <div className="flex-grow flex flex-col items-start">
-          <span className="text-semibold-s text-green-600 bg-green-50 border-custom-s border-gray-300 rounded-2xl py-px px-2">
-            {category[0]}
-          </span>
-          <div className="px-0.5">
-            <h3 className="text-semibold-m mt-2 mb-0.5">{title}</h3>
-            <p className="text-medium-m text-gray-400">
-              {questionListTitle ?? "함께 면접 스터디에 참여해보세요!"}
-            </p>
-            <div className="absolute bottom-5 left-6 text-medium-r flex flex-col">
-              <span className="text-gray-600">{host}</span>
-              <span className="text-gray-black flex gap-2 items-center">
-                <FaUserGroup className="text-green-400" />
-                참여자 {participants}/{maxParticipants}명
-              </span>
+    <li className={`relative h-52 bg-white rounded-custom-m px-5 py-6 transition-all duration-200 ease-in-out hover:-translate-y-1.5 border-custom-s border-gray-200
+      ${inProgress === false ? "hover:shadow-16 hover:ring-1 hover:ring-green-200" : ""}`}>
+      <Link
+        onClick={onEnter}
+        to={`/channel/${id}`}
+        className="w-full h-full"
+      >
+        <div className="flex flex-col justify-between">
+          <div className="flex-grow flex flex-col items-start">
+            <span className="text-semibold-s text-green-600 bg-green-50 border-custom-s border-gray-300 rounded-2xl py-px px-2">
+              {category[0]}
+            </span>
+            <div className="px-0.5">
+              <h3 className="text-semibold-m mt-2 mb-0.5">{title}</h3>
+              <p className="text-medium-m text-gray-400">
+                {questionListTitle ?? "함께 면접 스터디에 참여해보세요!"}
+              </p>
+              <div className="absolute bottom-5 left-6 text-medium-r flex flex-col">
+                <span className="text-gray-600">{host}</span>
+                <span className="text-gray-black flex gap-2 items-center">
+                  <FaUserGroup className="text-green-400" />
+                  참여자 {participants}/{maxParticipants}명
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </li>
   );
 };
 
