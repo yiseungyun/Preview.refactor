9157f2a1d577d8899f749841c29569bdea84172c
yiseungyun <seungxxyun@gmail.com>
2025-04-17 16:24:27 +0900

    refactor: sm에서 특정 열에 비디오 개수 조정

--- frontend/src/pages/channel/components/VideoContainer.tsx
+++ frontend/src/pages/channel/components/VideoContainer.tsx
@@ -36,7 +36,7 @@ const layoutConfig: LayoutConfig[] = [{
 }, {
   breakpoint: "sm",
   maxVideoPerRow: {
-    1: 1, 2: 2, 3: 1, 4: 1, 5: 2
+    1: 1, 2: 1, 3: 1, 4: 1, 5: 2
   }
 }, {
   breakpoint: "md",
